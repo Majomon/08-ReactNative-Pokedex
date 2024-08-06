38ead5a79227b168daa74ba4b1196c7a3fdab5a7
Mauri <mauri.monzon91@gmail.com>
2024-08-06 11:47:47 -0300

    Theme dark y light

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,16 +1,13 @@
 import 'react-native-gesture-handler';
 
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {ThemeContextProvider} from './presentation/context/ThemeContext';
 import {StackNavigator} from './presentation/navigator/StackNavigator';
-import {PaperProvider} from 'react-native-paper';
 
 export const PokedexApp = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContextProvider>
+      <StackNavigator />
+    </ThemeContextProvider>
   );
 };
