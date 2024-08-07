86499e99751daa7fa25c7f8a63d4d9c6011655c4
Mauri <mauri.monzon91@gmail.com>
2024-08-07 16:55:32 -0300

    Precargar informacion

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -5,9 +5,19 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {globalTheme} from '../../../config/theme/global-theme';
 import {Pokemon} from '../../../domain/entities/pokemon';
 import {PokemonCard} from '../../components/pokemons/PokemonCard';
+import {useQuery} from '@tanstack/react-query';
+import {getPokemonNameWithId} from '../../../actions/pokemons';
 
 export const SearchScreen = () => {
   const {top} = useSafeAreaInsets();
+
+  const {isLoading, data: pokemonNameList = []} = useQuery({
+    queryKey: ['pokemons', 'all'],
+    queryFn: () => getPokemonNameWithId(),
+  });
+
+  console.log(pokemonNameList);
+
   return (
     <View style={[globalTheme.globalMargin, {paddingTop: top + 10}]}>
       <TextInput
